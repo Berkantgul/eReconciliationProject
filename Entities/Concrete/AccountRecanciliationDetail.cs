7e2d80121dbfe20657bb797104fa0e8e83f33ec0
Berkant Gul <berkantgul25@gmail.com>
2023-02-18 20:01:55 +0300

    Entities Layer is done

---
+++ Entities/Concrete/AccountRecanciliationDetail.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class AccountRecanciliationDetail : IEntity
+    {
+        public int Id { get; set; }
+        public int AccountRecanciliationId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int CurrencyId { get; set; }
+        public decimal CurrencyDebit { get; set; }
+        public decimal CurrencyCredit { get; set; }
+    }
+}
