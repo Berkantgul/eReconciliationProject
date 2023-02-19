b0e547f69402dcdb01c267e04cc8166490ba637a
Berkant Gul <berkantgul25@gmail.com>
2023-02-19 10:20:25 +0300

    DataAccess Layer is done

---
+++ DataAccess/Concrete/EntityFramework/Context/ContextDb.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-UROTH5AA;Database=eRecanciliationDb;Integrated Security=true");
+        }
+        public DbSet<AccountRecanciliation> AccountRecanciliations { get; set; }
+        public DbSet<AccountRecanciliationDetail> AccountRecanciliationDetails { get; set; }
+        public DbSet<BaBsRecanciliation> BaBsRecanciliations { get; set; }
+        public DbSet<BaBsRecanciliationDetail> BaBsRecanciliationDetails { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        public DbSet<MailParameter> MailParameters { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
