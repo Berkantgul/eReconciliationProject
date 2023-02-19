3592ffa62e26eb651e563a3f3360a18a6f64cf52
Berkant Gul <berkantgul25@gmail.com>
2023-02-19 14:18:15 +0300

    transaction result list transactions completed

---
+++ Business/Concrete/CompanyManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Contans;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CompanyManager : ICompanyService
+    {
+        private readonly ICompanyDal _companyDal;
+
+        public CompanyManager(ICompanyDal companyDal)
+        {
+            _companyDal = companyDal;
+        }
+
+        public IResult Add(Company company)
+        {
+            _companyDal.Add(company);
+            return new SuccessResult(Messages.AddedCompany);
+        }
+
+        public IDataResult<List<Company>> GetAll()
+        {
+            return new SuccessDataResult<List<Company>>(_companyDal.GetAll());
+        }
+    }
+}
